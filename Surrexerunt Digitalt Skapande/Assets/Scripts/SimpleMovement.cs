b4805bd129b4141ff55940b96ec61c7f07dc3156
pontanVB <pontus.w.m@gmail.com>
2018-09-10 14:52:44 +0200

    Parralax
    Draft för parralax bakgrund samt simpel movement

---
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    protected Rigidbody2D rb2d;
+    [SerializeField]
+    protected float speed;
+    
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        ControllObject();
+    }
+
+    public void ControllObject()
+    {
+        rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+
+    }
+}
