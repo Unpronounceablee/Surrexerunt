dad907ec93e20385f483c228c23e4252338731ec
pontanVB <pontus.w.m@gmail.com>
2018-12-17 15:55:17 +0100

    CameraFixAndPrefabs

--- Surrexerunt Digitalt Skapande/Assets/Scripts/CameraController.cs
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class CameraController : MonoBehaviour
     Vector3 refer = Vector3.zero;
     private float smoothing;
     [SerializeField] private float defaultSmoothing;
+    [SerializeField] private float smootingReturn;
+
     //[SerializeField] private float smootingWhileDashing;
 
 
@@ -22,9 +25,9 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        WhenToSmoth();
         RoudedOffAxis();
         MoveCamera();
-        OnPlayerDash();
     }
 
     void MoveCamera()
@@ -43,11 +46,36 @@ public class CameraController : MonoBehaviour
             controlOffset = 1;
     }
 
+    void WhenToSmoth()
+    {
+        if (controlOffset < 0 && Input.GetAxis("Horizontal") > 0)
+        {
+            smoothing = defaultSmoothing;
+        }
+        else if (controlOffset > 0 && Input.GetAxis("Horizontal") < 0)
+        {
+            smoothing = defaultSmoothing;
+        }
+        else if (smoothing > 1)
+        {
+            smoothing = smoothing / smootingReturn;
+        }
+
+    }
+
     void OnPlayerDash()
     {
         if (player.GetComponent<PlayerMovement>().dashState == PlayerMovement.DashState.Dashing)
             smoothing = defaultSmoothing * player.GetComponent<PlayerMovement>().slowTimeScale;
         else
             smoothing = defaultSmoothing;
+    }
+
+    void Restart()
+    {
+        if (Input.GetButton("Back"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
