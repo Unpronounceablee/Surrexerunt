cab700ee9825ef0b78a739b20c567781770d660a
pontanVB <pontus.w.m@gmail.com>
2019-01-21 08:18:36 +0100

    DashDmgProgress

---
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/PlayerScripts/DashDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashDamage : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    [SerializeField] private int dashDmg;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void DealDamageInDash(Collider2D enemy)
+    {
+        if (playerMovement.dashState == PlayerMovement.DashState.Dashing && enemy.tag == "Boss")
+        {
+            enemy.GetComponent<BossManager>().health -= dashDmg;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DealDamageInDash(collision);
+    }
+}
