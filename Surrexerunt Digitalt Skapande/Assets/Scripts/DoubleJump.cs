4fe7ff8bc99397a6257b836c06bb0a269c388758
Unpronounceablee <unpronounceablee@gmail.com>
2019-03-18 15:53:33 +0100

    Added double jump and basically finished the boss functionaly

---
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/DoubleJump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            if (Input.GetButtonDown("Jump")) {
+                other.GetComponent<PlayerMovement>().isGrounded = true;
+                other.GetComponent<PlayerMovement>().DoubleJump();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
