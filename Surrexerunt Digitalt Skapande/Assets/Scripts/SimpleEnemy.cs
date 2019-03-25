15ed25e1a7bdfb01a868fb4788e13d27efc57161
pontanVB <pontus.w.m@gmail.com>
2019-03-25 15:11:11 +0100

    PlayerDamagedOnKnockback

--- Surrexerunt Digitalt Skapande/Assets/Scripts/SimpleEnemy.cs
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/SimpleEnemy.cs
@@ -7,6 +7,9 @@ public class SimpleEnemy : MonoBehaviour {
     [SerializeField]float speed, dirTime;
     float effectiveDirTime;
     bool changeDir = false;
+    private GameObject player;
+    private bool hasCollided;
+
 
 	void Update () {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
@@ -23,4 +26,37 @@ public class SimpleEnemy : MonoBehaviour {
             effectiveDirTime -= Time.deltaTime;
         }
 	}
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void PlayerInteraction()
+    {
+        if (player.GetComponent<PlayerMovement>().dashState == PlayerMovement.DashState.Dashing)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            player.GetComponent<PlayerMovement>().dashState = PlayerMovement.DashState.Knockback;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player && !hasCollided)
+        {
+            PlayerInteraction();
+            StartCoroutine(Wait());
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        hasCollided = true;
+        yield return new WaitForSeconds(0.5f);
+        hasCollided = false;
+    }
 }
