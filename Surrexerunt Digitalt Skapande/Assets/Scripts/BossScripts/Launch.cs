8c888853b2dde50f98eea840e901f9070363efea
Unpronounceablee <unpronounceablee@gmail.com>
2019-03-26 12:51:59 +0100

    Added More Thorns

--- Surrexerunt Digitalt Skapande/Assets/Scripts/BossScripts/Launch.cs
+++ Surrexerunt Digitalt Skapande/Assets/Scripts/BossScripts/Launch.cs
@@ -18,7 +18,6 @@ public class Launch : MonoBehaviour {
 
     void Update()
     {
-
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (transform.position.x == target.x && transform.position.y == target.y)
         {
